47286af8767a6e4821760011e5990f22a6d1f56e
Dmitrii Gorbachev <dgorbacev742@gmail.com>
2024-04-04 18:27:59 +0300

    add filled models Result and ResultPresentation

--- src/Services/Results/Results.Api/Models/ResultPresentation.cs
+++ src/Services/Results/Results.Api/Models/ResultPresentation.cs
@@ -2,5 +2,15 @@
 {
     public class ResultPresentation
     {
+        public ResultPresentation(string exercise, float weightKg, int numberOfRepetitions)
+        {
+            Exercise = exercise;
+            WeightKg = weightKg;
+            NumberOfRepetitions = numberOfRepetitions;
+        }
+
+        public string Exercise { get; }
+        public float WeightKg { get; }
+        public int NumberOfRepetitions { get; }
     }
 }
--- src/Services/Results/Results.Domain/Models/Result.cs
+++ src/Services/Results/Results.Domain/Models/Result.cs
@@ -2,6 +2,16 @@
 {
     public class Result
     {
-        
+        public Result(string exercise, float weightKg, int numberOfRepetitions)
+        {
+            Exercise = exercise;
+            WeightKg = weightKg;
+            NumberOfRepetitions = numberOfRepetitions;
+        }
+
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Exercise { get; }
+        public float WeightKg { get; }
+        public int NumberOfRepetitions { get; }
     }
 }
