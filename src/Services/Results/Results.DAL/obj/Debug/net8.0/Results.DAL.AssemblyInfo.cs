a26fe9e3d695506f651e8ed4531e014e58fee0b4
Dmitrii Gorbachev <dgorbacev742@gmail.com>
2024-04-04 21:40:06 +0300

    Merge branch 'feature/addDbPostqreSql' into develop
    # Conflicts:
    #	.vs/ProjectEvaluation/traininghelper.metadata.v7.bin
    #	.vs/ProjectEvaluation/traininghelper.projects.v7.bin
    #	.vs/TrainingHelper/DesignTimeBuild/.dtbcache.v2
    #	.vs/TrainingHelper/FileContentIndex/de077cd7-da82-439a-ade8-92f6707624c1.vsidx
    #	.vs/TrainingHelper/FileContentIndex/fb610846-9335-4402-8bcc-0a72479a8df4.vsidx
    #	.vs/TrainingHelper/v17/.futdcache.v2
    #	.vs/TrainingHelper/v17/.suo
    #	.vs/TrainingHelper/v17/DocumentLayout.json
    #	src/Services/Results/Results.Api/Program.cs
    #	src/Services/Results/Results.Api/Results.Api.csproj
    #	src/Services/Results/Results.Api/obj/Debug/net8.0/Results.Api.AssemblyInfo.cs
    #	src/Services/Results/Results.Api/obj/Debug/net8.0/Results.Api.AssemblyInfoInputs.cache
    #	src/Services/Results/Results.Api/obj/Debug/net8.0/Results.Api.csproj.AssemblyReference.cache
    #	src/Services/Results/Results.Api/obj/Results.Api.csproj.nuget.dgspec.json
    #	src/Services/Results/Results.Api/obj/project.assets.json
    #	src/Services/Results/Results.Api/obj/project.nuget.cache
    #	src/Services/Results/Results.Application/obj/Debug/net8.0/Results.Application.csproj.AssemblyReference.cache
    #	src/Services/Results/Results.DAL/obj/Debug/net8.0/Results.DAL.AssemblyInfo.cs
    #	src/Services/Results/Results.DAL/obj/Debug/net8.0/Results.DAL.AssemblyInfoInputs.cache
    #	src/Services/Results/Results.DAL/obj/Debug/net8.0/Results.DAL.csproj.AssemblyReference.cache
    #	src/Tests/Results/ResultsServiceTests/obj/Debug/net8.0/ResultsServiceTests.AssemblyInfo.cs
    #	src/Tests/Results/ResultsServiceTests/obj/Debug/net8.0/ResultsServiceTests.AssemblyInfoInputs.cache
    #	src/Tests/Results/ResultsServiceTests/obj/Debug/net8.0/ResultsServiceTests.csproj.AssemblyReference.cache

--- src/Services/Results/Results.DAL/obj/Debug/net8.0/Results.DAL.AssemblyInfo.cs
+++ src/Services/Results/Results.DAL/obj/Debug/net8.0/Results.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Results.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fe55e0e17b8b86d93f6a45a16ef369c26be849")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+541dbd3f08d0dd03c7b18c9650c8273f766d3c10")]
 [assembly: System.Reflection.AssemblyProductAttribute("Results.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Results.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Tests/Results/ResultsServiceTests/obj/Debug/net8.0/ResultsServiceTests.AssemblyInfo.cs
+++ src/Tests/Results/ResultsServiceTests/obj/Debug/net8.0/ResultsServiceTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResultsServiceTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fe55e0e17b8b86d93f6a45a16ef369c26be849")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+541dbd3f08d0dd03c7b18c9650c8273f766d3c10")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResultsServiceTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResultsServiceTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
