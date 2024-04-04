826ddf83cb20bc9cfe7c9b4e4e8c37d37526f7b8
Dmitrii Gorbachev <dgorbacev742@gmail.com>
2024-04-04 18:31:07 +0300

    add AutoMapper

---
+++ src/Services/Results/Results.Api/ApiMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Results.Api.Models;
+using Results.Domain.Models;
+
+namespace Results.Api
+{
+    public class ApiMappingProfile : Profile
+    {
+        public ApiMappingProfile() 
+        {
+            CreateMap<ResultPresentation, Result>().ReverseMap();
+        }
+    }
+}
--- src/Services/Results/Results.Api/Program.cs
+++ src/Services/Results/Results.Api/Program.cs
@@ -1,8 +1,12 @@
+using Results.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(ApiMappingProfile));
 
 var app = builder.Build();
 
